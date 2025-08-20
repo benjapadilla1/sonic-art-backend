c1c6aa9a0ac852d2388f41f7ed6cf4381cc0e309
benjapadilla1 <benja.padilla@outlook.com.ar>
2025-08-20 17:44:41 -0300

    fix: adding www to cors

--- src/app.ts
+++ src/app.ts
@@ -6,17 +6,22 @@ import routes from "./routes";
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://sonicartlab.com",
+  "https://www.sonicartlab.com",
+];
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "https://sonicartlab.com", // tu frontend deployado
-    ],
+    origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+
+app.use(cors());
 
 app.use(express.json());
 app.use(morgan("dev"));
